f71ad3f6a6caf0848d3030a93b874da4f47a0a6f
NPC-MARIMO <shivangbhaiisgreat@gmail.com>
2024-11-05 13:18:31 +0530

    eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,9 +3,9 @@ module.exports = {
     browser: true,
     es2021: true,
   },
-  extends: [
-    'plugin:react/recommended',
-    'airbnb',
+  "extends": [
+    "next",
+    "next/core-web-vitals"
   ],
   parserOptions: {
     ecmaFeatures: {
